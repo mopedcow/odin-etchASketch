a1a41f46f7d6b82d214163ad8eb46810f57ee269
mopedcow <188090362+mopedcow@users.noreply.github.com>
2025-03-18 16:06:46 +0000

    Create flexible grid using only Flexbox. Each section in the grid created using DOM manipulation. Styled using CSS.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//lol
+const container = document.querySelector('#container');
+
+const gridLength = 16;
+
+for (i = gridLength; i > 0; i--) {
+    const column = document.createElement('div');
+    column.classList.add('column');
+    container.append (column);
+    for (j = gridLength; j > 0; j--) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        column.append(square);
+    }
+}
